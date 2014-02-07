a64596acdcde8122c77b4ba226e0702d3035c781
Димо Петров <dimo.petrof@gmail.com>
2014-02-07 21:18:42 +0200

    Innitial commint

---
+++ KnightsOfCSharpia/KnightsOfCSharpia/Creature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsOfCSharpia
+{
+    class Creature
+    {
+        string name;
+        int healthPoints = 0;
+        int mana = 0;
+        int level = 0;
+        int nextLevel;
+        int eplorer
+    }
+}
