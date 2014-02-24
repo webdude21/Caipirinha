b0bde963816e296b05b2d2b5528b2a5879a87c9d
NasC0 <Spirital@gmail.com>
2014-02-24 12:56:30 +0200

    Re-added AttackLog struct

---
+++ KnightsOfCSharpia/KnnightsOfCSharpiaLib/Engine/AttackLog.cs
@@ -0,0 +1,21 @@
+﻿namespace KnightsOfCSharpiaLib.Engine
+{
+    using System;
+
+    public struct AttackLog
+    {
+
+        public AttackLog(DamageType type, uint damage)
+            :this()
+        {
+            this.Damage = damage;
+            this.DamageType = type;
+        }
+
+        public DamageType DamageType { get; set; }
+
+        public uint Damage { get; set; }
+
+        public string AttackInformation { get; set; }
+    }
+}
