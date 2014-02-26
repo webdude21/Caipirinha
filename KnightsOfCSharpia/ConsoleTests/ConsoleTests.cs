7b1f1bb3e434d90ef65009ff9df87dd5408a8153
Димо Петров <dimo.petrof@gmail.com>
2014-02-26 21:35:48 +0200

    Removed unused Namespaces

--- KnightsOfCSharpia/ConsoleTests/ConsoleTests.cs
+++ KnightsOfCSharpia/ConsoleTests/ConsoleTests.cs
@@ -1,10 +1,5 @@
 ﻿using KnightsOfCSharpiaLib.Common;
-using KnightsOfCSharpiaLib.Items;
 using KnightsOfCSharpiaLib.Creatures;
-using KnightsOfCSharpiaLib.Engine;
-using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
 
 class ConsoleTests
 {
